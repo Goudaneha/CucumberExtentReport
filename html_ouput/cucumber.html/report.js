a2c0764e76607a508d1fef24f944ffc07af3deba
goudaneha <janagoudarani@gmail.com>
2020-06-01 04:01:12 -0700

    Implementing cucumber selenium code with extent report

---
+++ html_ouput/cucumber.html/report.js
@@ -0,0 +1,223 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/nehaashokj/E-CommerceTest/src/test/java/E-Commerce.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Welcome to online shopping",
+  "description": "",
+  "id": "welcome-to-online-shopping",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Welcome to E-Commerce Website",
+  "description": "",
+  "id": "welcome-to-online-shopping;welcome-to-e-commerce-website",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "User is on Registration page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User clicks on Signin \u003c\"signin\"\u003e",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User has to provide \"\u003cemail\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "click on \u003c\"createacc\"\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User enters \"\u003cfirstname\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "name \"\u003clastname\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "clicks on \u003c\"Reg\"\u003e",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 13,
+  "name": "",
+  "description": "",
+  "id": "welcome-to-online-shopping;welcome-to-e-commerce-website;",
+  "rows": [
+    {
+      "cells": [
+        "signin",
+        "email",
+        "createacc",
+        "firstname",
+        "lastname",
+        "Reg"
+      ],
+      "line": 14,
+      "id": "welcome-to-online-shopping;welcome-to-e-commerce-website;;1"
+    },
+    {
+      "cells": [
+        "signin",
+        "apples77@gamil.com",
+        "createacc",
+        "Smith",
+        "Tom",
+        "Reg"
+      ],
+      "line": 15,
+      "id": "welcome-to-online-shopping;welcome-to-e-commerce-website;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Welcome to E-Commerce Website",
+  "description": "",
+  "id": "welcome-to-online-shopping;welcome-to-e-commerce-website;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "User is on Registration page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User clicks on Signin \u003c\"signin\"\u003e",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User has to provide \"apples77@gamil.com\"",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "click on \u003c\"createacc\"\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User enters \"Smith\"",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "name \"Tom\"",
+  "matchedColumns": [
+    4
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "clicks on \u003c\"Reg\"\u003e",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_is_on_Registration_page()"
+});
+formatter.result({
+  "duration": 63092364100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "signin",
+      "offset": 24
+    }
+  ],
+  "location": "StepDefinition.user_clicks_on_Signin(String)"
+});
+formatter.result({
+  "duration": 5562388500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "apples77@gamil.com",
+      "offset": 21
+    }
+  ],
+  "location": "StepDefinition.user_has_to_provide(String)"
+});
+formatter.result({
+  "duration": 2183154300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "createacc",
+      "offset": 11
+    }
+  ],
+  "location": "StepDefinition.click_on(String)"
+});
+formatter.result({
+  "duration": 4288197400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Smith",
+      "offset": 13
+    }
+  ],
+  "location": "StepDefinition.user_enters(String)"
+});
+formatter.result({
+  "duration": 3606100400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Tom",
+      "offset": 6
+    }
+  ],
+  "location": "StepDefinition.name(String)"
+});
+formatter.result({
+  "duration": 47007100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Reg",
+      "offset": 12
+    }
+  ],
+  "location": "StepDefinition.clicks_on(String)"
+});
+formatter.result({
+  "duration": 20090982700,
+  "error_message": "org.openqa.selenium.NoSuchElementException: Unable to locate element: //sp[contains(text(),\u0027Registe\u0027)]\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027BRTL059\u0027, ip: \u0027192.168.43.245\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_201\u0027\nDriver info: org.openqa.selenium.firefox.FirefoxDriver\nCapabilities {acceptInsecureCerts: true, browserName: firefox, browserVersion: 76.0, javascriptEnabled: true, moz:accessibilityChecks: false, moz:buildID: 20200429185419, moz:geckodriverVersion: 0.26.0, moz:headless: false, moz:processID: 17904, moz:profile: C:\\Users\\nehaashokj\\AppData..., moz:shutdownTimeout: 60000, moz:useNonSpecCompliantPointerOrigin: false, moz:webdriverClick: true, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, platformVersion: 10.0, rotatable: false, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: e2d86216-3eca-449b-a0cf-69b61fd190b2\n*** Element info: {Using\u003dxpath, value\u003d//sp[contains(text(),\u0027Registe\u0027)]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.utility.test.CommonMethods.clickmethod(CommonMethods.java:12)\r\n\tat com.StepDEfinition.test.StepDefinition.clicks_on(StepDefinition.java:51)\r\n\tat ✽.Then clicks on \u003c\"Reg\"\u003e(C:/Users/nehaashokj/E-CommerceTest/src/test/java/E-Commerce.feature:11)\r\n",
+  "status": "failed"
+});
+});
---
+++ src/main/java/com/ruuner/test/TestRunner.java
@@ -0,0 +1,63 @@
+package com.ruuner.test;
+
+import java.io.File;
+
+import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+
+import com.vimalselvam.cucumber.listener.Reporter;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "C:\\Users\\nehaashokj\\E-CommerceTest\\src\\test\\java\\E-Commerce.feature",
+		//plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.json"},//extent report
+		glue ={"com.StepDEfinition.test"},
+		format = {"pretty","html:html_ouput/cucumber.html","json:json_output/cucumber.json",
+				"html:target/cucumber-reports/cucumber-pretty",
+				"json:target/cucumber-reports/CucumberTestReport.json",
+				"rerun:target/cucumber-reports/rerun.txt"
+				},
+			plugin={"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html"},
+		
+		monochrome = true,
+		strict = true,
+		dryRun = false
+		)
+public class TestRunner {
+	@AfterClass
+	public static void setup()
+	{
+	Reporter.loadXMLConfig(new File("C:\\Users\\nehaashokj\\E-CommerceTest\\src\\main\\java\\com\\ruuner\\test\\extent-config.xml"));
+	//Reporter.setSystemInfo("Test User", System.getProperty("user.name"));
+	Reporter.setSystemInfo("User Name", "NJ");
+	Reporter.setSystemInfo("Application Name", "Test App ");
+	Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
+	Reporter.setSystemInfo("Environment", "Production");
+	Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
+	}
+
+//	private TestNGCucumberRunner testNGCucumberRunner;
+//	 
+//    @BeforeClass(alwaysRun = true)
+//    public void setUpClass() throws Exception {
+//        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+//    }
+// 
+//    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+//    public void feature(CucumberFeatureWrapper cucumberFeature) {
+//        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+//    }
+// 
+//    @DataProvider
+//    public Object[][] features() {
+//        return testNGCucumberRunner.provideFeatures();
+//    }
+// 
+//    @AfterClass(alwaysRun = true)
+//    public void tearDownClass() throws Exception {
+//        testNGCucumberRunner.finish();
+//    }
+}
